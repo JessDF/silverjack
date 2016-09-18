643f680b8b0f7507df8fa22aa1b5167a31495ac2
JessieDF <jdowding@csumb.edu>
2016-09-18 19:53:36 +0000

    Fixed image link

--- game.php
+++ game.php
@@ -1,38 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Arrays Review</title>
-        <link rel="stylesheet" href="css/style.css" tyype="text/css" />
-    </head>
-    
-    <body>
-        
-        <h1>Silverjack Review</h1>
-       
-       <?php
+<?php
        
        function displayRandCard()
        {
            $suits = array("clubs", "diamonds", "hearts","spades");
            $randomSuitIndex = rand(0,3);
            $randomSuit = $suits[$randomSuitIndex];
-           echo  "<img src = 'Pics/$randomSuit/" . rand(1, 13) . ".png' />";
+           echo  "<img src = '../img/cards/$randomSuit/" . rand(1, 13) . ".png' />";
            
            $deck = array();
            for($i = 1; $i <= 52; $i++)
            {
                $deck[] = $i;
            }
-           
            shuffle($deck);
-           
            $card = array_pop($deck);
-           
            echo $card . "<br />";
        }
        
        
        ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Arrays Review</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css" />
+    </head>
+    
+    <body>
+        
+        <h1>Silverjack Review</h1>
        
        
        
