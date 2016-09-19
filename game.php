aa64bce6e470a7ad10e8ee7b938969cd8d7327e5
Mateo Sixtos <msixtos@csumb.edu>
2016-09-19 01:28:54 +0000

    ading our photos

--- game.php
+++ game.php
@@ -6,15 +6,19 @@
            $suits = array("clubs", "diamonds", "hearts","spades");
            $randomSuitIndex = rand(0,3);
            $randomSuit = $suits[$randomSuitIndex];
+          
            echo  "<img src = 'img/cards/$randomSuit/" . rand(1, 13) . ".png' />";
-           
+           echo  "<img src = 'img/mateo.jpg' />";
+           echo  "<img src = 'img/jessie.jpg' />";
+           echo  "<img src = 'img/Garrett.jpg' />";
            $deck = array();
            for($i = 1; $i <= 52; $i++)
            {
                $deck[] = $i;
            }
            shuffle($deck);
-           $card = array_pop($deck);
+          //
+          $card = array_pop($deck);
            echo $card . "<br />";
        }
        
